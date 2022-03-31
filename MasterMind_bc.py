8419a01cc534e57c4de6e7eabd67f6ae0efd42d7
Mohammad Alattar <78165884+MohammadAlattar@users.noreply.github.com>
2022-03-31 06:37:54 +0300

    Update MasterMind_bc.py

--- MasterMind_bc.py
+++ MasterMind_bc.py
@@ -1,45 +1,54 @@
-# first create random numbers 
+import os
 import random
-numbers = []
-efforts = 0 
-def RandomNumbers(): 
- for i in range(4):
-     r=random.randrange(0,9)
-     if r not in numbers: 
-       numbers.append(r)
-     else:
-       numbers.clear
-       RandomNumbers()  
-     
- 
+import string
 
-# create game 
-def play():
-  global efforts 
-  efforts +=1 
-  bulls = 0 
-  cows=0
-#  print(numbers)
-  inputNum = input("guess 4 numbers -> ")
-  gus=[]
-
-# input 4 digit number to gus and check if the numbers bulls or not   
-  for bu in range(4):
-    gus.append(int(inputNum[bu])) 
-  for i in range(4):
-     if gus[i] == numbers[i]:
-      bulls+=1
-
-# check the cows : if the number in gus[cw] is exist in numbers[i] at any index of list 
-  for cw in range(4):
+print("-----------------------------------------")
+print("\t\tMenu")
+print("-----------------------------------------")
+print("Enter code using characters.")
+print("You should constant of 4 characters from A to F ")
+print("EXAMPLE : ABCD")
+print("If you don't know how to play the game, read the file README")
+print("-----------------------------------------")
+result_str = []
+attempts= 0 
+def get_random_string():
+    letters = string.ascii_uppercase
     for i in range(4):
-     if gus[cw] == numbers[i]:
-      cows += 1
-  print(f"A{bulls}{cows}B")
-  if bulls == 4 : 
-    print(f"you win after :{efforts} efforts")
-  else:
-    play()
+        rand_str= ''.join(random.choice('ABCDEF'))
+        result_str.append(rand_str)
+    
+    return result_str
 
-RandomNumbers()
-play()
+
+def create_game():
+    global attempts 
+    attempts +=1
+    right_position = 0 
+    wrong_position = 0 
+    # print(result_str)    
+    input_str = input("Your guess : ")
+    guess = []
+    for count_str in range(4):
+        guess.append(input_str[count_str].upper())  
+    
+    for guess_str_w in range(4):
+        for check in range(4):
+            if guess[guess_str_w] == result_str[check]:
+                wrong_position +=1  
+    for guess_str in range(4):    
+        if guess[guess_str] == result_str[guess_str]: 
+            right_position +=1                    
+    print("Right Position is : ", right_position)    
+    print("wrong Position is : ", wrong_position)    
+
+    if right_position == 4 :
+        print("congratulations you win after ".upper(),attempts)
+        
+    else : 
+        create_game()   
+
+
+
+get_random_string()
+create_game()
